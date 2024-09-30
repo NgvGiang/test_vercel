9e182516aa1fcefa6a99c0524cf51bcff05e5097
Nguyen Van Giang <giangnv.22bi13132@usth.edu.vn>
2024-09-30 10:45:30 +0700

    test

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+const sendButton = document.querySelector('.btn-outline-secondary');
+
+// Thực hiện sự kiện khi người dùng click vào nút
+sendButton.addEventListener('click', function() {
+    alert('Button clicked!');
+});
+
